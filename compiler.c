0ff7e37371b52cad78d23399cfdd35c4c8b0c80a
CheRongtian <145754269+CheRongtian@users.noreply.github.com>
2025-11-20 01:16:14 -0600

    first commit

---
+++ compiler.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <string.h>
+
+int token; // current token
+char *src, *old_src; // pointer to source code string
+int poolsize; // default size fo text/data/stack
+int line; // line number
+
+/* get next one, ignore blanket */
+void next()
+{
+    token = *src++;
+    return;
+}
+
+/* analysis one expression */
+void expression(int level)
+{
+    // do nothing ~
+}
+ /* entrance of grammar analysis, analyzing the whole c-file*/
+void program()
+{
+    next(); // get next token
+    while(token > 0)
+    {
+        printf("token is : %c\n", token);
+        next();
+    }
+}
+
+/* entrance of VM*/
+int eval()
+{
+    return 0;
+    // do nothing also ~
+}
+
+int main(int argc, char **argv)
+{
+    int i, fd;
+    
+    argc--;
+    argv++;
+
+    poolsize = 256 * 1024; // arbitary size
+    line = 1;
+
+    if((fd = open(*argv, 0)) < 0)
+    {
+        printf("could not open (%s)\n", *argv);
+        return -1;
+    }
+
+    if(!(src = old_src = malloc(poolsize)))
+    {
+        printf("could not malloc (%d) for source area\n", poolsize);
+        return -1;
+    }
+
+    // read the source file
+    if((i = read(fd, src, poolsize - 1)) <= 0)
+    {
+        printf("read() returned %d\n", i);
+        return -1;
+    }
+
+    src[i] = 0; // add EOF character
+    close(fd);
+
+    program();
+    return eval();
+}
