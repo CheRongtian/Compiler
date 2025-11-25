44d5301df779a7555469328812d0e3c755990ca7
CheRongtian <145754269+CheRongtian@users.noreply.github.com>
2025-11-24 22:40:51 -0600

    add one test.c file, change type of var to fit 64-bit Mac

--- compiler.c
+++ compiler.c
@@ -2,15 +2,17 @@
 #include <stdlib.h>
 #include <memory.h>
 #include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-int token; // current token
+long token; // current token
 char *src, *old_src; // pointer to source code string
 int poolsize; // default size fo text/data/stack
 int line; // line number
 
-int *text; // text segment
-int *old_text; // for dump next segment
-int *stack; // stack
+long *text; // text segment
+long *old_text; // for dump next segment
+long *stack; // stack
 char *data; // data segment
 /*
 +------------------------+
@@ -32,14 +34,17 @@ char *data; // data segment
 
 // Registers 
 
-int *pc; // progrom counting, address of memory, inside of the address is the command for next execution
-int *sp; // pointer->top of stack, lowering
-int *bp; // base pointer -> some place of the stack, function() will use it
-int ax; // store the result after executing one command
-int cycle;
+long *pc; // progrom counting, address of memory, inside of the address is the command for next execution
+long *sp; // pointer->top of stack, lowering
+long *bp; // base pointer -> some place of the stack, function() will use it
+long ax; // store the result after executing one command
+long cycle;
 
 // Instructions
-enum { IMM, LT, GT, LC, LI, SC, SI, PUSH};
+enum { LC, LI, SC, SI,
+       IMM, PUSH, JMP, JZ, JNZ, CALL, ENT, ADJ, LEV, LEA, 
+       OR, XOR, AND, EQ, NE, LT, LE, GT, GE, SHL, SHR, ADD, SUB, MUL, DIV, MOD,
+       EXIT, OPEN, CLOS, READ, PRTF, MALC, MSET, MCMP};
 
 /* get next one, ignore blanket */
 void next()
@@ -59,7 +64,7 @@ void program()
     next(); // get next token
     while(token > 0)
     {
-        printf("token is : %c\n", token);
+        printf("token is : %ld\n", token);
         next();
     }
 }
@@ -67,20 +72,100 @@ void program()
 /* entrance of VM*/
 int eval()
 {
-    int op, *tmp;
+    long op, *tmp;
     while(1)
     {
+        op = *pc++;
         if(op == IMM) ax = *pc++; // load immediate value to ax
         else if(op == LC) ax = *(char *)ax; // load character to ax, address in ax
-        else if(op == LI) ax = *(int *)ax; // load integer to ax, address in ax
+        else if(op == LI) ax = *(long *)ax; // load integer to ax, address in ax
         else if(op == SC) ax = *(char *)*sp++ = ax; // save character to address, value in ax, address on stack
-        else if(op == SI) *(int *)*sp++ = ax; // save integer to address, value in ax, address on stack
+        else if(op == SI) *(long *)*sp++ = ax; // save integer to address, value in ax, address on stack
         // MOVE
+        
         else if(op == PUSH) *--sp = ax; // push the value of ax onto the stack
         // PUSH
+        
+        else if(op == JMP) pc = (long *)*pc; // jump to the address
+        // JUMP
+        
+        else if(op == JZ) pc = ax ? pc + 1: (long *) *pc; // jump if ax == 0
+        else if(op == JNZ) pc = ax ? (long *) *pc : pc + 1; // jump if ax != 0
+        // JZ/JNZ
+        
+        else if(op == CALL) 
+        {
+            *--sp = (long) (pc + 1); 
+            pc = (long *) *pc; 
+            // call subroutine
+        }
+        // else if(op == RET) pc = (int *) *sp ++;
+        // CALL
+
+        else if(op == ENT)
+        {
+            *--sp = (long) bp;
+            bp = sp;
+            sp = sp - *pc++;
+            // make new stack frame
+        }
+        // ENT
+
+        else if(op == ADJ) sp = sp + *pc++; // add esp, <size>
+        // ADJ
+
+        else if (op == LEV) 
+        {
+            sp = bp;
+            bp = (long *) *sp++;
+            pc = (long *) *sp++;
+            // remove call frame and PC
+        }
+        // LEV (includes RET function)
+
+        else if(op == LEA) ax = (long)(bp + *pc++); // load address for arguements
+        // LEA
+
+        else if(op == OR) ax = *sp++ | ax;
+        else if(op == XOR) ax = *sp++ ^ ax;
+        else if(op == AND) ax = *sp++ & ax;
+        else if(op == EQ) ax = *sp++ == ax;
+        else if(op == NE) ax = *sp++ != ax;
+        else if(op == LT) ax = *sp++ < ax;
+        else if(op == LE) ax = *sp++ <= ax;
+        else if(op == GT) ax = *sp++ > ax;
+        else if(op == GE) ax = *sp++ >= ax;
+        else if(op == SHL) ax = *sp++ << ax;
+        else if(op == SHR) ax = *sp++ >> ax;
+        else if(op == ADD) ax = *sp++ + ax;
+        else if(op == SUB) ax = *sp++ - ax;
+        else if(op == MUL) ax = *sp++ * ax;
+        else if(op == DIV) ax = *sp++ / ax;
+        else if(op == MOD) ax = *sp++ % ax;
+
+        else if(op == EXIT)
+        {
+            printf("exit(%ld)\n", *sp);
+            return *sp;
+        }
+        else if(op == OPEN) ax = open((char *)sp[1], sp[0]);
+        else if(op == CLOS) ax = close (*sp);
+        else if(op == READ) ax = read(sp[2], (char *)sp[1], *sp);
+        else if(op == PRTF)
+        {
+            tmp = sp + pc[1];
+            ax = printf((char *)tmp[-1], tmp[-2], tmp[-3], tmp[-4], tmp[-5], tmp[-6]);
+        }
+        else if(op == MALC) ax = (long)malloc(*sp);
+        else if(op == MSET) ax = (long)memset((char *)sp[2], sp[1], *sp);
+        else if(op == MCMP) ax = memcmp((char *)sp[2], (char *)sp[1], *sp);
+        else
+        {
+            printf("unknown instructuions: %ld\n", op);
+            return -1;
+        }
     }
     return 0;
-    // do nothing also ~
 }
 
 int main(int argc, char **argv)
@@ -122,13 +207,13 @@ int main(int argc, char **argv)
         return -1;
     }
 
-    if(data != malloc(poolsize))
+    if(!(data = malloc(poolsize)))
     {
         printf("could not malloc(%d) size for data area", poolsize);
         return -1;
     }
 
-    if(stack != malloc(poolsize))
+    if(!(stack = malloc(poolsize)))
     {
         printf("could not malloc(%d) size for stack area", poolsize);
         return -1;
@@ -138,9 +223,21 @@ int main(int argc, char **argv)
     memset(data, 0, poolsize);
     memset(stack, 0, poolsize);
 
-    bp = sp = (int *)((int)stack + poolsize);
+    bp = sp = (long *)((long)stack + poolsize);
     ax = 0;
 
-    program();
+    // test "10 + 20"
+    i = 0;
+    text[i++] = IMM;
+    text[i++] = 10;
+    text[i++] = PUSH;
+    text[i++] = IMM;
+    text[i++] = 20;
+    text[i++] = ADD;
+    text[i++] = PUSH;
+    text[i++] = EXIT;
+    pc = text;
+
+    // program();
     return eval();
 }
---
+++ test.c
@@ -0,0 +1,6 @@
+# include <stdio.h>
+// test file
+int main()
+{
+    return 0;
+}
