1f27c4d0b82d41e3474e1567e0e212e01b2a48da
CheRongtian <145754269+CheRongtian@users.noreply.github.com>
2025-12-16 23:22:16 -0600

    Add .gitignore and cleanup build artifacts

--- build/CMakeFiles/compiler.dir/parse.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/compiler.dir/parse.c.o: \
-  /Users/cherongtian/Desktop/demo\ projects/Compiler/parse.c \
-  /Users/cherongtian/Desktop/demo\ projects/Compiler/next.h \
-  /Users/cherongtian/Desktop/demo\ projects/Compiler/parse.h
