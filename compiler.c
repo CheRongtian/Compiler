bd38bfccb35636d410925627f94a0fddbbd4ccf8
CheRongtian <145754269+CheRongtian@users.noreply.github.com>
2025-12-19 00:59:17 -0600

    add a global_declaration() function

--- compiler.c
+++ compiler.c
@@ -110,6 +110,95 @@ void match(long tk)
     next();
 }
 
+long basetype; // the type of a declaration, make it global for convenience
+long expr_type; // the type of an expression
+
+void global_declaration()
+{
+    // global_declaration ::= enum_decl | variable_decl | function_decl
+    // enum_decl ::= 'enum'[id] '{' id ['=' 'num'} '}'
+    // variable_decl ::= type {'*'} id { ',' {'*'} id } ';'
+    // function_decl ::= type {'*'} id '(' parameter_decl ')' '{' body_decl '}'
+
+    long type; // tmp, actual type for variable
+    long i; // tmp
+
+    basetype = INT;
+
+    // parse enum, this should be treated alone
+
+    if(token == Enum)
+    {
+        // enum[id] { a = 10, b = 20, ... }
+        match(Enum);
+        if(token != '{') match(Id); // skip the [id] part
+        if(token == '{')
+        {
+            // parse the assign part
+            match('{');
+            enum_declaration();
+            match('}');
+        }
+        match(';');
+        return;
+    }
+
+    // parse type information
+    if(token == Int) match(Int);
+    else if(token == Char)
+    {
+        match(Char);
+        basetype = CHAR;
+    }
+
+    // parse the comma seperated variable declaration
+    while(token != ';' && token != '}')
+    {
+        type = basetype;
+        // parse pointer type, note that there may exist `int ****;`
+        while(token == Mul)
+        {
+            match(Mul);
+            type += PTR;
+        }
+
+        if(token != Id)
+        {
+            // invalid declaration
+            printf("%d: bad global declaration\n", line);
+            exit(-1);
+        }
+        
+        if(current_id[Class])
+        {
+            // identifier exists
+            printf("%d: duplicate global declaration\n", line);
+            exit(-1);
+        }
+
+        match(Id);
+        current_id[Type] = type;
+
+        if(token == '(')
+        {
+            current_id[Class] = Fun;
+            current_id[Value] = (long)(text+1); // the memory address of function
+            function_declaration();
+        }
+        
+        else
+        {
+            // variable declaration
+            current_id[Class] = Glo; // global variable
+            current_id[Class] = (long)data; // assign memory address
+            data += sizeof(long);
+        }
+
+        if(token == ',') match(',');
+    }
+    next();
+}
+
 /* analysis one expression */
 void expression(int level)
 {
@@ -119,11 +208,14 @@ void expression(int level)
 void program()
 {
     next(); // get next token
+    /*
     while(token > 0)
     {
         printf("token is : %ld\n", token);
         next();
     }
+    */
+   global_declaration();
 }
 
 int main(int argc, char **argv)
